020362bd9ca688ff9afc6e5d27c0eed428314aca
Tismon Varghese <head.php@vstmobility.com>
2020-09-26 17:43:53 +0530

    final touches

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -5,40 +5,46 @@
     <img alt="Inbox logo" width="400px" src="../assets/inbox.jpg">
     <!-- /logo -->
 
-    <!-- search -->
-    <div class="ml-5 mr-5">
-      <b-row>
-        <b-col cols="3"><b-form-input v-model="search" placeholder="Search"></b-form-input></b-col>
-        <b-col class="text-left">
-          <b-button @click="doSearch" variant="primary">Search</b-button>
-          &nbsp;
-          <b-button @click="clearSearch" variant="secondary">Clear</b-button>
-        </b-col>
-      </b-row>
-      
-    </div>
-    <!-- /search -->
-    <p></p>
-    <!-- emails -->
-    <div class="email-wrapper ml-5 mr-5 mt-10">
-      <b-table id="emails_table" responsive :items="items" :per-page="perPage" :current-page="currentPage" ></b-table>
-    </div>
-    <!-- /emails -->
+    <div v-if="items.length > 0">
+      <!-- search -->
+      <div class="ml-5 mr-5">
+        <b-row>
+          <b-col cols="3"><b-form-input v-model="search" placeholder="Search"></b-form-input></b-col>
+          <b-col class="text-left">
+            <b-button @click="doSearch" variant="primary">Search</b-button>
+            &nbsp;
+            <b-button @click="clearSearch" variant="secondary">Clear</b-button>
+          </b-col>
+        </b-row>
+        
+      </div>
+      <!-- /search -->
+      <p></p>
+      <!-- emails -->
+      <div class="email-wrapper ml-5 mr-5 mt-10">
+        <b-table id="emails_table" responsive :items="items" :per-page="perPage" :current-page="currentPage" ></b-table>
+      </div>
+      <!-- /emails -->
 
-    <!-- pagination -->
-    <div class="ml-5">
-      <paginate
-        :page-count="Math.trunc(totalEmails/perPage)"
-        :page-range="3"
-        :margin-pages="2"
-        :click-handler="getEmails"
-        :prev-text="'Prev'"
-        :next-text="'Next'"
-        :container-class="'pagination'"
-        :page-class="'page-item'">
-      </paginate>
+      <!-- pagination -->
+      <div class="ml-5">
+        <paginate
+          :page-count="Math.trunc(totalEmails/perPage)"
+          :page-range="3"
+          :margin-pages="2"
+          :click-handler="getEmails"
+          :prev-text="'Prev'"
+          :next-text="'Next'"
+          :container-class="'pagination'"
+          :page-class="'page-item'">
+        </paginate>
+      </div>
+      <!-- /pagination -->
     </div>
-    <!-- /pagination -->
+
+    <div v-else>
+      No email found.
+    </div>
 
   </div>
 </template>
